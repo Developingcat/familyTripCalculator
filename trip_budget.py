d624678eb0c1808a7f296a46bb3becb6d659b282
Hannah <148793520+Developingcat@users.noreply.github.com>
2025-02-04 15:49:27 +0000

    worked more on the trip budget

--- trip_budget.py
+++ trip_budget.py
@@ -17,15 +17,15 @@ BUDGET = 6000
 budget_total = print("Your current budget is: 6,000$")
 
 airfare_cost = float(input("Please enter the airfare cost, per person: $ "))
-
-day_total = float(input("Please enter the amount of days you'll be staying: "))
-
+# print(float(airfare_cost))
+day_total = input("Please enter the amount of days you'll be staying: ")
+# print(float(day_total))
 food_daily = float(input("Please enter the daily food budget estimate: $ "))
-
+# print(float(int((food_daily))
 souvenir_budget = float(input("Please enter the desired souvenir budget estimate: $ "))
-
+# print(float(int(souvenir_budget)))
 excursion_cost = float(input("Please enter the desired excursion cost estimate: $ "))
-
+# print(float(int(excursion_cost)))
 # If statement with MATH 
 
 
@@ -42,19 +42,22 @@ excursion_cost = float(input("Please enter the desired excursion cost estimate: 
 # Reference 
 
 
+
 added_summary = (airfare_cost * 4) + (food_daily * 4 * day_total) + souvenir_budget + excursion_cost
 
 
-if added_summary >= BUDGET: 
-    cut_cost = added_summary - BUDGET
+
+if added_summary >= 6000: 
+    cut_cost = added_summary - 6000
     print("That exceeds your budget!" ,added_summary, "You should cut your cost by," ,cut_cost, )
-elif added_summary == BUDGET: 
+elif added_summary == 6000: 
     print("That is the exact price... tread cautiously.")
-elif added_summary < BUDGET:
-    left_over = added_summary * -1 - BUDGET 
+elif added_summary < 6000:
+    left_over = added_summary - 6000  * -1.00
     print("You have $" ,left_over, "left for other things!")
 else: 
     print("Wuh oh." )
+
 
 
 
